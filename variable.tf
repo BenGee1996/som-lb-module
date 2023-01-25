9a98dcb58ce794b9ba9582ea1a5c700504baec9a
Ben Graham <ben.graham@ecs.co.uk>
2023-01-25 16:31:35 +0000

    amended variable.tf

--- variable.tf
+++ variable.tf
@@ -4,74 +4,9 @@ variable "default-region" {
 }
 
 // VPC
-variable "vpc_cidr_block" {
-  type = string
-}
 variable "vpc_id" {
   type = string
   default = null
-}
-variable "subnet_cidr_block_a" {
-  type = string
-}
-variable "subnet_cidr_block_b" {
-  type = string
-}
-variable "az" {
-  type = map(any)
-  default = {
-    a = "eu-west-1a"
-    b = "eu-west-1b"
-
-  }
-}
-variable "routetable_cidr_block" {
-  type = string
-}
-
-//CLUSTER
-variable "ecr_name" {
-  type = string
-}
-variable "cluster_name" {
-  type = string
-}
-variable "task_family" {
-  type = string
-}
-variable "container_definitions" {
-  type = string
-}
-variable "task_memory" {
-  type = string
-}
-variable "task_cpu" {
-  type = string
-}
-
-//IAM
-variable "execution_role_name" {
-  type = string
-}
-variable "policy_arn" {
-  type = string
-}
-
-//SERVICE
-variable "service_name" {
-  type = string
-}
-variable "launch_type" {
-  type = string
-}
-variable "desired_count" {
-  type = string
-}
-variable "min_healthy_percent" {
-  type = string
-}
-variable "container_port" {
-  type = string
 }
 
 //SECURITY GROUP
@@ -102,9 +37,6 @@ variable "lb_sg_name" {
 
 //TARGET GROUP
 variable "tg_name" {
-  type = string
-}
-variable "tg_port" {
   type = string
 }
 
